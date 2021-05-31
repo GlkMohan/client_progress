49e6eefdcbbb5db55fed39c0ecc8c2599efe7c65
Akhil <68549118+AkhilChappa@users.noreply.github.com>
2021-05-31 20:49:02 +0530

    Add files via upload

---
+++ client_progress/dashboard.php
@@ -0,0 +1,59 @@
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+	  <link rel="stylesheet" href="assets/css/style.css">
+	<title>Dashboard</title>
+	<style type="text/css">
+	html {
+		font-family: arial;
+	}
+</style>
+</head>
+<body>
+
+<header>Client Page</header>
+<div class="container-fluid" style="background-image: url('images/back.jpg');width: 100%;height: 100%;background-repeat: no-repeat;position: fixed;overflow-y: scroll;">
+	
+
+		<div class="row project_card">
+		  <div class="column card_data col-xs-12 col-sm-12 col-lg-12 col-md-12">
+		    <h2>Project 1</h2>
+		    <p>Some text..</p>
+		  </div>
+		  <div class="column card_data col-xs-12 col-sm-12 col-lg-12 col-md-12">
+		    <h2>Project 2</h2>
+		    <p>Some text..</p>
+		  </div>
+		  <div class="column card_data col-xs-12 col-sm-12 col-lg-12 col-md-12">
+		    <h2>Project 3</h2>
+		    <p>Some text..</p>
+		  </div>
+		  <div class="column card_data col-xs-12 col-sm-12 col-lg-12 col-md-12">
+		    <h2>Project 4</h2>
+		    <p>Some text..</p>
+		  </div>
+		  <div class="column card_data col-xs-12 col-sm-12 col-lg-12 col-md-12">
+		    <h2>Project 5</h2>
+		    <p>Some text..</p>
+		  </div>
+		  <div class="column card_data col-xs-12 col-sm-12 col-lg-12 col-md-12">
+		    <h2>Project 6</h2>
+		    <p>Some text..</p>
+		  </div>
+		</div>
+
+
+</div>
+
+
+
+
+
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</body>
+</html>
